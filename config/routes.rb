eab1e425808b2c59e710bf56440b5dbf11ef1947
Tianxiao <tianxiaom@citrix.com>
2016-08-16 13:26:41 +0800

    add api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :carts
-  resources :votes
-  resources :users
-  resources :books do
-    collection do
-      post 'search'
+  scope '/api' do
+    resources :carts
+    resources :votes
+    resources :users
+    resources :books do
+      collection do
+        post 'search'
+      end
     end
+    resources :vote_sessions
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-  resources :vote_sessions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
